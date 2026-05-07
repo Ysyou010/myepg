93000ed70b9553b16194a0dc446d403b229fb04b
Ysyou010 <131797487+Ysyou010@users.noreply.github.com>
2026-05-07 15:57:02 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 import os
 # 원본 주소 대신 '회원님의 깃허브 아이디'가 들어간 주소로 변경합니다.
-os.system('pip install -U git+https://github.com/ysyou010/epg2xml.git')
+os.system('pip install -U git+https://github.com/Ysyou010/epg2xml.git')
